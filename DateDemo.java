b25ba8393e5e05d312225457590325d79595d9e5
Majid Hamsi <majidhamsi@gmail.com>
2020-09-02 13:15:08 +0800

    Add DateDemo.java file

---
+++ DateDemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class DateDemo{
+	public static void main(String[] args){
+		Date d = new Date();
+		System.out.println("current date : "+ d);
+		Date d2 = new Date(2323223232L);
+         	System.out.println("Date represented is "+ d2 ); 
+	}
+}
